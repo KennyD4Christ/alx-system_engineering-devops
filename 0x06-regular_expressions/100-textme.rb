6a7086528be9d76b27bcfc7a491ee88b5d05abea
KennyD4Christ <kennyd4christ@gmail.com>
2024-02-27 12:15:44 +0100

    100-textme.rb

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,10 +1,3 @@
 #!/usr/bin/env ruby
+puts ARGV[0].scan(/\[from:(.*?)\] \[to:(.*?)\] \[flags:(.*?)\]/).join(",")
 
-# Read each line from the input file
-File.open(ARGV[0], "r").each do |line|
-  # Match the sender, receiver, and flags using regex
-  match_data = line.match(/\[from:(.*?)\] \[to:(.*?)\] \[flags:(.*?)\]/)
-            
-  # Output the matched sender, receiver, and flags
-  puts "#{match_data[1]},#{match_data[2]},#{match_data[3]}"
-end
