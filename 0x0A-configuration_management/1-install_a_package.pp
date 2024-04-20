cabed3f06ad2cbf6c3bdfa227c8e66bca54abd15
KennyD4Christ <kennyd4christ@gmail.com>
2024-04-20 10:51:05 +0100

    All

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,11 +1,6 @@
-# Install Flask 2.1.0
-package { 'Flask':
-  ensure    => '2.1.0',
-  provider  => 'pip3',
-}
+# This Puppet manifest installs Flask version 2.1.0 using pip3
 
-# Install Werkzeug 2.1.1
-package { 'Werkzeug':
-  ensure    => '2.1.1',
-  provider  => 'pip3',
+package { ['Flask', 'Werkzeug']:
+ ensure  => '2.1.0',
+ provider => 'pip3',
 }
