07b05c44a84ff86d43ad08199583aafb6b11565b
KennyD4Christ <kennyd4christ@gmail.com>
2024-04-20 16:02:15 +0100

    All

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,7 +1,11 @@
 # Define variable for key path (if not directly specified in the template)
-$key_path = '/home/kennyd/.ssh/school'
-
-file { '/etc/ssh/sshd_config':
+file { '/home/kennyd/.ssh/config':
   ensure  => present,
-  content => template('/home/kennyd/alx-system_engineering-devops/0x0B-ssh/templates/sshd_config.erb'),
+  mode    => '0600',
+  owner   => 'kennyd',
+  group   => 'kennyd',
+  content => "Host my_server
+  HostName 100.26.122.117
+  IdentityFile ~/.ssh/school
+  PasswordAuthentication no\n",
 }
