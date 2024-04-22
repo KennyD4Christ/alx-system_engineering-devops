6e7e89b8726a8daa80c4735d169b8b9358f7bcbe
KennyD4Christ <kennyd4christ@gmail.com>
2024-04-22 09:39:41 +0100

    All

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,12 +1,25 @@
 # Define variable for key path (if not directly specified in the template)
+
 file_line { 'Turn off passwd auth':
-  path  => '/etc/ssh/sshd_config',
-  line  => 'PasswordAuthentication no',
-  match => '^#?PasswordAuthentication',
+  ensure => present,
+  path   => '/home/kennyd/.ssh/config',
+  line   => 'PasswordAuthentication no',
 }
 
 file_line { 'Declare identity file':
-  path  => '/etc/ssh/ssh_config',
-  line  => 'IdentityFile ~/.ssh/school',
-  match => '^#?IdentityFile',
+  ensure => present,
+  path   => '/home/kennyd/.ssh/config',
+  line   => 'IdentityFile ~/.ssh/school',
+}
+
+file_line { 'Add server configuration':
+  ensure => present,
+  path   => '/home/kennyd/.ssh/config',
+  line   => 'Host my_server',
+}
+
+file_line { 'Add server IP':
+  ensure => present,
+  path   => '/home/kennyd/.ssh/config',
+  line   => '  HostName 100.26.122.117',
 }
