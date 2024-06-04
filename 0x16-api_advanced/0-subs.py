d5e1f0f5bf675c2bc52f2fad7511ecd193e2c045
KennyD4Christ <kennyd4christ@gmail.com>
2024-06-04 14:00:29 +0100

    0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,27 +8,13 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-    """
-    Queries the Reddit API and returns the number of subscribers for a
-    given subreddit.
-    If an invalid subreddit is given, the function returns 0.
-    """
-    url = f'https://www.reddit.com/r/{subreddit}/about.json'
-    headers = {'User-Agent': 'MyAPIProject/0.0.1'}
-
-    try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        if response.status_code == 200:
-            data = response.json()
-            return data['data']['subscribers']
-        else:
-            return 0
-    except requests.RequestException:
+    """Return the total number of subscribers on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "Mozilla/5.0"}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
+        data = response.json()
+        subscribers = data['data']['subscribers']
+        return subscribers
+    else:
         return 0
-
-
-# Example usage
-if __name__ == "__main__":
-    subreddit = input("Enter the name of the subreddit: ")
-    subscribers = number_of_subscribers(subreddit)
-    print(f"Number of subscribers in r/{subreddit}: {subscribers}")
