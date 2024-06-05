cf024f62385d5126ac484e57c3297c5adb0e5e51
KennyD4Christ <kennyd4christ@gmail.com>
2024-06-05 17:51:56 +0100

    0-strace_is_your_friend.pp

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,14 +1,6 @@
-# This Puppet manifest ensures the correct ownership and permissions for /var/www/html/index.php and restarts Apache if necessary.
+# fixes bad "phpp" extensions to "php" in "wp-settings.php".
 
-file { '/var/www/html/index.php':
-  ensure => file,
-  owner  => 'www-data',
-  group  => 'www-data',
-  mode   => '0644',
-}
-
-exec { 'restart_apache':
-  command     => '/usr/sbin/service apache2 restart',
-  refreshonly => true,
-  subscribe   => File['/var/www/html/index.php'],
+exec{'fix-wordpress':
+  command => 'sed -i a/phpp/php/g /var/www/htlm/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
 }
