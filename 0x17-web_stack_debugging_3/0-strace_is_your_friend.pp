0aebe7ee5953e76f7139c5f7b7ad8d6f28d2ffd2
James Daniel <danieljameskehinde@yahoo.com>
2024-06-06 10:40:26 +0000

    Adjusted

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 # fixes bad "phpp" extensions to "php" in "wp-settings.php".
 
 exec{'fix-wordpress':
-  command => 'sed -i a/phpp/php/g /var/www/htlm/wp-settings.php',
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
   path    => '/usr/local/bin/:/bin/'
 }
